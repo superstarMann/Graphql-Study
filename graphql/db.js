71cbe677bfd5b3de092b79355f5f875685cbf523
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2020-06-14 16:18:19 +0900

    14

--- graphql/db.js
+++ graphql/db.js
@@ -29,16 +29,26 @@ let movies = [
 export const getMovies = () => movies;
 
 export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === String(id));
+    const filteredMovies = movies.filter(movie => movie.id = id);
     return filteredMovies[0];
-};
+}; 
 
 export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+    const cleanedMovies = movies.filter(movie => movie.id != id);
     if (movies.length > cleanedMovies.length){
         movies = cleanedMovies;
         return true;
     }else {
         return false;
     }
+};
+
+export const addMovie = (name, score) =>{
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
 };
